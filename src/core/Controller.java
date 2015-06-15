61f625f1a154b6f04f7a52446a4ccf073adfb1e1
oak <k.o.alon@gmail.com>
2015-06-15 17:54:24 +0300

    set controller as thread

--- src/core/Controller.java
+++ src/core/Controller.java
@@ -1,15 +1,22 @@
 package core;
 
 import java.util.Collection;
+import java.util.List;
 
 import ui.Renderable;
 
-public class Controller implements Choosable<Item>, Renderable<Item>{
+public class Controller  implements Choosable<Item>, Renderable<Item>, Runnable {
 	private Collection<Item> list;
+	private boolean changed;
 	public Controller() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public Controller(List<Item> items) {
+		this.setDataSource(items);
+		this.changed = true;
+	}
+
 	public void setDataSource (Collection<Item> list) {
 		this.list = list;
 	}
@@ -23,12 +30,32 @@ public class Controller implements Choosable<Item>, Renderable<Item>{
 	@Override
 	public void choose(Item item) {
 		// TODO Auto-generated method stub
+		this.changed = true;
 		
 	}
 	@Override
 	public Collection<Item> dataSource() {
 		// TODO Auto-generated method stub
 		return this.list;
+	}
+	
+	public void run(){
+
+        while(true) {
+                try {
+                	if (this.changed){ 
+                		this.render(this.dataSource());
+                		this.changed = false;
+                	} else{
+                		System.out.println("nothing has been changed");
+                	}
+                	
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        }
 	}
 	
 	
--- src/core/CoreMain.java
+++ src/core/CoreMain.java
@@ -7,17 +7,21 @@ public class CoreMain {
 	private static int NUMBER_OF_ITEMS = 6;
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		Controller controller = new Controller();
+		
 		List<Item> items = new ArrayList<Item>(); 
 		
 		for (int i = 0 ; i < NUMBER_OF_ITEMS ; i++) {
 			items.add(new Item());
 		}
 		
-		controller.setDataSource(items);
-		controller.render(controller.dataSource());
+		
+		Controller controller = new Controller(items);
+		new Thread(controller).start();
+//		controller.run();
+		Thread.sleep(400);
+		controller.choose(items.get(0));
 	}
 
 }
